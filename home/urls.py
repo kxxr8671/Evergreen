5d6a536baef2cbcaf275f4e0cd90c323decdfe76
karthikdotnet <karthikdotnet@CORP.AMAZONWORKSPACES.COM>
2019-10-29 22:26:14 -0400

    Added Home page Nav bar links.

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+    path('Services/', views.services, name = 'services'),
+    path('Products/', views.products, name = 'products'),
+    path('About Us/', views.about_us, name = 'about_us'),
+    path('Contact Us/', views.contact_us, name = 'contact_us')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'home.html', {})
+
+def services(request):
+   return render(request, 'services.html', {})
+
+def products(request):
+   return render(request, 'products.html', {})
+
+def about_us(request):
+   return render(request, 'about_us.html', {})
+
+def contact_us(request):
+   return render(request, 'contact_us.html', {})
