e097f32d345a91842ffa009ff39127d1a4d73f2f
karthikdotnet@CORP.AMAZONWORKSPACES.COM <karthikdotnet@CORP.AMAZONWORKSPACES.COM>
2019-11-24 21:27:20 -0500

    added email feature to contact us page.

--- egreen_trade_source/settings.py
+++ egreen_trade_source/settings.py
@@ -127,3 +127,10 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'testing@example.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template.loader import get_template
 
 # Create your views here.
 def home(request):
@@ -14,4 +17,30 @@ def about_us(request):
    return render(request, 'about_us.html', {})
 
 def contact_us(request):
-   return render(request, 'contact_us.html', {})
+    if request.method == 'POST':
+        first_name = request.POST.get(
+            'first_name'
+        , '')
+        email = request.POST.get(
+            'email'
+        , '')
+
+        message = request.POST.get('message', '')
+        template = get_template('contact_template.txt')
+        context = {
+            'first_name': first_name,
+            'email': email,
+            'message': message,
+        }
+        content = template.render(context)
+
+        email = EmailMessage(
+            "New contact form submission",
+            content,
+            "Your website" +'',
+            ['youremail@gmail.com'],
+            headers = {'Reply-To': email }
+        )
+        email.send()
+        return redirect('contact_us')
+    return render(request, 'contact_us.html', {})
