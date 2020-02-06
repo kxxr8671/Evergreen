16d489b615eac650a3f340e99d1b0632d7fa0874
kxxr8671 <karthikdotnet@gmail.com>
2020-02-05 22:13:02 -0500

    First commit from my local machine.

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('Services/', views.services, name = 'services'),
     path('Products/', views.products, name = 'products'),
-    path('About Us/', views.about_us, name = 'about_us'),
-    path('Contact Us/', views.contact_us, name = 'contact_us')
+    path('AboutUs/', views.about_us, name = 'about_us'),
+    path('ContactUs/', views.contact_us, name = 'contact_us')
 ]
